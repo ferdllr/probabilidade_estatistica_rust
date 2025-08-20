d6de99ea225a7a61a448face320c0beb58d2f7f8
Fernando <jorneuu@proton.me>
2025-08-20 16:31:44 -0300

    struct Calc adicionado

---
+++ src/calc.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+
+pub struct Calc;
+
+impl Calc {
+    pub fn amplitude(dados: &[f64]) -> Option<f64> {
+        if dados.is_empty() {
+            return None;
+        }
+        let mut max = dados[0];
+        let mut min = dados[0];
+
+        for &dado in dados {
+            if max < dado {
+                max = dado;
+            }
+            if min > dado {
+                min = dado;
+            }
+        }
+        Some(max - min)
+    }
+
+    pub fn moda(dados: &[i32]) -> Option<i32> {
+        let mut map = HashMap::new();
+        let mut bigger = 0;
+        let mut moda = None;
+        for &dado in dados {
+            *map.entry(dado).or_insert(0) += 1;
+        }
+        for (&n, &v) in &map {
+            if v > bigger {
+                bigger = v;
+                moda = Some(n);
+            }
+        }
+        if bigger <= 1 {
+            return None;
+        }
+        moda
+    }
+
+    pub fn mediana(dados: &[f64]) -> f64 {
+        let mut lista = dados.to_vec();
+        lista.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        if lista.len() % 2 != 0 {
+            let pos = lista.len() / 2;
+            lista[pos]
+        } else {
+            let pos1 = lista.len() / 2 - 1;
+            let pos2 = lista.len() / 2;
+            (lista[pos1] + lista[pos2]) / 2.0
+        }
+    }
+
+    pub fn media_aritimetica(dados: &[f64]) -> f64 {
+        let mut somatoria = 0.0;
+        for dado in dados {
+            somatoria += dado;
+        }
+        somatoria / dados.len() as f64
+    }
+
+    pub fn probabilidade(espaco_amostral: &[i32], condicao: &dyn Fn(i32) -> bool) -> f64 {
+        let mut dados_favoraveis = 0;
+
+        for dado in espaco_amostral {
+            if condicao(*dado) {
+                dados_favoraveis += 1;
+            }
+        }
+
+        dados_favoraveis as f64 / espaco_amostral.len() as f64
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+mod calc;
+use calc::Calc;
+fn main() {
+    println!("===========");
+    println!("--probabilidade--");
+    let dados_possiveis = [1, 2, 3, 4, 5, 6];
+    println!(
+        "a probabilidade de cair um numero par é de: {}%",
+        Calc::probabilidade(&dados_possiveis, &|n| n % 2 == 0) * 100.0
+    );
+    println!(
+        "a probabilidade de cair um numero impar é de: {}%",
+        Calc::probabilidade(&dados_possiveis, &|n| n % 2 != 0) * 100.0
+    );
+    println!(
+        "a probabilidade de cair um numero maior que 4 é de: {}%",
+        Calc::probabilidade(&dados_possiveis, &|n| n > 4) * 100.0
+    );
+
+    println!("===========");
+    println!("--media--");
+    let notas = [5.1, 8.5, 10.0, 5.5, 3.2, 7.9, 8.2, 2.2];
+    println!(
+        "a média das notas é: {:.2}",
+        Calc::media_aritimetica(&notas)
+    );
+
+    println!("===========");
+    println!("--mediana--");
+    let salarios = [1000.0, 1200.0, 850.0, 1540.0, 20.0, 2200.0, 1950.0, 5000.0];
+    println!("a mediana é: {}", Calc::mediana(&salarios));
+
+    println!("===========");
+    println!("--moda--");
+    let intnums = [2, 5, 3, 3, 8, 7, 10, 120, 5, 3, 5, 5];
+    println!("existe: a moda é: {:?}", Calc::moda(&intnums));
+    let semmoda = [1, 2, 3, 4, 5, 6, 7];
+    println!("não existe: a moda é: {:?}", Calc::moda(&semmoda));
+
+    println!("===========");
+    println!("--amplitude--");
+    let amp_array1 = [7.0, 7.0, 7.0, 7.0, 7.0];
+    let amp_array2 = [5.0, 6.0, 7.0, 8.0, 9.0];
+    println!("1a lista: amplitude: {:?}", Calc::amplitude(&amp_array1));
+    println!("2a lista: amplitude: {:?}", Calc::amplitude(&amp_array2));
+}
